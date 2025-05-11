02a74ab6801b53fb79c83e3e826bb547237348f3
AMANTRIPATHI12 <amantripathi2244@gmail.com>
2025-05-11 15:43:30 +0530

    test update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import viteCompression from 'vite-plugin-compression';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteCompression()],
-  base: '/Ace-Developer/', // Ensures correct paths after deployment
 })
