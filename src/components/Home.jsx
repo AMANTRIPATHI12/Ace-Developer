ee8d312915111d703c851c3b500647efa0939ad2
AMANTRIPATHI12 <amantripathi2244@gmail.com>
2025-05-11 11:20:43 +0530

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,14 @@ const Home = () => {
     <section className="relative w-full h-screen overflow-hidden">
       {/* Background Image */}
       <img
-        src="./images/Home.webp"
-        alt="Ace Developer Background"
-        loading="eager"
-        className="absolute inset-0 w-full h-full object-cover z-0"
-        width="1920"
-        height="1080"
-      />
+      src="/Ace-Developer/images/Home.webp"
+      alt="Ace Developer Background"
+      loading="eager"
+      className="absolute inset-0 w-full h-full object-cover z-0"
+      width="1920"
+      height="1080"
+    />
+
 
       {/* Overlay Text */}
       <div className="relative z-10 w-full h-full flex items-center justify-center text-center bg-black/50">
